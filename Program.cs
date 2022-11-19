7f6be990811596c8c3d7f350d7870090729f0539
leskovskiy <116192490+leskovskiy@users.noreply.github.com>
2022-11-19 14:25:10 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,74 +1,50 @@
-﻿
-//Напишите программу, которая на вход принимает два числа и выдаёт, 
-//какое число большее, а какое меньшее.
-void Zadacha2 ()
+﻿// Напишите программу, которая принимает на вход 
+//трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+void Zadacha10()
 {
-Console.WriteLine("Введите первое число: ");
-int first = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число: ");
-int second = Convert.ToInt32(Console.ReadLine());
-
-if (first > second )
-    Console.WriteLine(first + " больше " + second);
-else 
-    Console.WriteLine(first + " меньше " + second);
-}
-Zadacha2();
-
-//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-void Zadacha4()
-{
-
-Console.WriteLine("Введите первое число: ");
+Console.WriteLine("Введите  число: ");
 int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число: ");
-int b = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите третье число: ");
-int c = Convert.ToInt32(Console.ReadLine());
- 
- int max = Math.Max(Math.Max(a, b) , c);
- int min = Math.Min(Math.Min(a, b) ,c );
+string str = Convert.ToString(a);
+Console.WriteLine("вторая цифра этого числа  "+ str[1]);
 
-Console.WriteLine("Максимальное число: " + max);
-Console.WriteLine("Минимальное число: "+ min );
-
-}
-Zadacha4();
-
-//Напишите программу, которая на вход принимает число и выдаёт,
-// является ли число чётным (делится ли оно на два без остатка).
-void Zadacha6()
-{
-    Console.WriteLine("Введите число : ");
-int number = Convert.ToInt32(Console.ReadLine());
-
-if (number % 2 ==0 )
-    Console.WriteLine(number + ": число четное  ");
-else 
-    Console.WriteLine(number + ": число нечетное  ");
-}
-Zadacha6();
-
-//Напишите программу, которая на вход принимает 
-//число (N > 0), а на выходе показывает все чётные числа от 1 до N.
-void Zadacha8()
-
-{  
-Console.WriteLine("Введите число: ");
-int N = Convert.ToInt32(Console.ReadLine());
-N = Math.Abs(N);
-int k = 1;
-
-while (N <= k);
     
-    if (k % 2 == 0)
-    k++;
-    Console.WriteLine( k +  " ");
-
-
-Console.WriteLine();
-
-
-
 }
-Zadacha8();
+Zadacha10();
+
+
+//Напишите программу, которая с помощью деления выводит третью цифру заданного числа или
+//сообщает, что третьей цифры нет.
+void Zadacha13()
+{
+    Console.WriteLine("Введите  число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+ string str = Convert.ToString(a);
+
+
+if (str.Length  >  2 )
+    
+        Console.WriteLine("Число " + a + " третья цифра " + str[2] );
+else
+    
+    Console.WriteLine("Трьетьей цифры нет в числе " + a );
+    
+}
+Zadacha13();
+
+//Напишите программу, которая принимает на
+//вход цифру, обозначающую день недели, и проверяет,
+//является ли этот день выходным
+
+void Zadacha15()
+{
+Console.WriteLine("Введите  число от 1 до 7 : ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+
+if (a  < 5 )
+    Console.WriteLine("Это будний день");
+else 
+    Console.WriteLine("Это выходной день ");
+}
+Zadacha15();
